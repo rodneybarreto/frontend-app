71c6b5df7ee5ee86f7d4dd4aa8c3738d271098db
Rodney Barreto <rodney.barreto@pge.ce.gov.br>
2021-03-20 11:04:44 -0300

    Add push to docker hub

--- docker.build-image.sh
+++ docker.build-image.sh
@@ -3,5 +3,8 @@
 ## Generate docker image
 docker build . -t rodneybarreto/minipets_frontend-app:1.0.0
 
+## Push to docker hub
+#docker push rodneybarreto/minipets_frontend-app:1.0.0
+
 ## Run container
-# docker run --name frontend-app -p 80:80 rodneybarreto/minipets_frontend-app:1.0.0
+#docker run --name frontend-app -p 80:80 rodneybarreto/minipets_frontend-app:1.0.0
