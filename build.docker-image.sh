af70fe9cfa53a4f811a4d9a74fe3f0604c994625
Rodney Barreto <rodney.barreto@pge.ce.gov.br>
2021-04-02 12:40:37 -0300

    Get comments from comments-api

--- build.docker-image.sh
+++ build.docker-image.sh
@@ -1,10 +1,10 @@
 #!/usr/bin/env sh
 
 ## Generate docker image
-docker build . -t rodneybarreto/minipets_frontend-app:1.0.0
+docker build . -t rodneybarreto/minipets_frontend-app:1.1.0
 
 ## Push to docker hub
-#docker push rodneybarreto/minipets_frontend-app:1.0.0
+#docker push rodneybarreto/minipets_frontend-app:1.1.0
 
 ## Run container
-#docker run --name frontend-app --network=host -p 8080:8080 rodneybarreto/minipets_frontend-app:1.0.0
+#docker run --name frontend-app --network=host -p 8080:8080 rodneybarreto/minipets_frontend-app:1.1.0
--- server.js
+++ server.js
@@ -14,6 +14,8 @@ const PRODUCTS_HOST = process.env.PRODUCTS_HOST || '0.0.0.0'
 const PRODUCTS_PORT = process.env.PRODUCTS_PORT || 8081
 const SHIPPING_HOST = process.env.SHIPPING_HOST || '0.0.0.0'
 const SHIPPING_PORT = process.env.SHIPPING_PORT || 8082
+const COMMENTS_HOST = process.env.COMMENTS_HOST || 'localhost'
+const COMMENTS_PORT = process.env.COMMENTS_PORT || 8083
 
 app.get('/', async (req, res) => {
   try {
@@ -21,9 +23,13 @@ app.get('/', async (req, res) => {
       host: SHIPPING_HOST,
       port: SHIPPING_PORT
     }
+    const comments = {
+      host: COMMENTS_HOST,
+      port: COMMENTS_PORT
+    }
 
     const result = await axios.get(`http://${PRODUCTS_HOST}:${PRODUCTS_PORT}/products`)
-    res.render('pages/index', { products: result.data, shipping })
+    res.render('pages/index', { products: result.data, shipping, comments })
   }
   catch(error) {
     console.log(error)
